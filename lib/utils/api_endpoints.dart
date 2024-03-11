1d82ee1d7e2835f0f3563259048c671c84742294
e.elhjoujy <e.elhjoujy@labelvie.ma>
2024-03-11 22:57:03 +0000

    endpoints

--- lib/utils/api_endpoints.dart
+++ lib/utils/api_endpoints.dart
@@ -3,6 +3,7 @@ class ApiEndPoints {
   static const String baseGateWayUrl = 'localhost:9075';
   static const String baseRegionUrl = 'localhost:8097';
   static const String userService = "/user-service";
+  static const String regionService = "/region-service";
   static const String loginEmail = '/api/v1/auth/authenticate';
   static const String getAllregions = '/api/v1/regions';
 }
